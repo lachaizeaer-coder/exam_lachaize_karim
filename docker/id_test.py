3a4c4c277b369c557d2b952ff6db0a0fc24d117b
Ubuntu <ubuntu@ip-172-31-45-25.eu-west-1.compute.internal>
2026-07-22 11:05:11 +0000

    move files for better content management

---
+++ docker/id_test.py
@@ -0,0 +1,50 @@
+import os
+import requests
+
+#### API adress
+api_address = ''
+#### API port
+api_port = 8000
+
+
+#### Request
+r = requests.get(
+    url='http://{address}:{port}/permissions'.format(address=api_address, port=api_port),
+    params= {
+        'username': 'alice',
+        'password': 'wonderland'
+    }
+)
+
+
+output = '''
+============================
+    Authentication test
+============================
+
+request done at "/permissions"
+| username="alice"
+| password="wonderland"
+
+expected result = 200
+actual restult = {status_code}
+
+==>  {test_status}
+
+'''
+
+
+# Request status
+status_code = r.status_code
+
+# Request result
+if status_code == 200:
+    test_status = 'SUCCESS'
+else:
+    test_status = 'FAILURE'
+print(output.format(status_code=status_code, test_status=test_status))
+
+# To log file
+if os.environ.get('LOG') == 1:
+    with open('api_test.log', 'a') as file:
+        file.write(output)
