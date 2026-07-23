9f01558809407bea0ae4be027f816623494cfbc7
Ubuntu <ubuntu@ip-172-31-45-25.eu-west-1.compute.internal>
2026-07-23 11:48:31 +0000

    first working version

--- docker/setup.sh
+++ docker/setup.sh
@@ -1,21 +1,12 @@
-# DOCKER SETUP
-docker volume create logs-volume
-
-# API
+#### API
 docker image pull datascientest/fastapi:1.0.0
-docker container run -p 8000:8000 --name reverent_hodgkin --mount type=volume,src=logs-volume, dst=/home/logs datascientest/fastapi:1.0.0
-docker network connect api-tests reverent_hodgkin
 
-#
-
-# FIRST TEST
+#### FIRST TEST
 docker image build . -f Dockerfile-id-test -t dockerfile-id-test:latest
-docker container run --mount type=volume,src=logs-volume,dst=/home/logs --network api-tests dockerfile-id-test:latest
-
-# SECOND TEST
+#### SECOND TEST
 docker image build . -f Dockerfile-credential-test -t dockerfile-credential-test:latest
-docker container run --mount type=volume,src=logs-volume,dst=/home/logs --network api-tests dockerfile-credential-test:latest
-
-# THIRD TEST
+#### THIRD TEST
 docker image build . -f Dockerfile-API-run-test -t dockerfile-api-run-test:latest
-docker container run --mount type=volume,src=logs-volume,dst=/home/logs --network api-tests dockerfile-api-run-test:latest
+
+#### RUN DOCKER COMPOSE
+docker compose -f docker-compose.yml up
