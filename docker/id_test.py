302c93be84177d722122989fc9b86d36ba5bb194
Ubuntu <ubuntu@ip-172-31-45-25.eu-west-1.compute.internal>
2026-07-23 08:06:25 +0000

    code adjustement

--- docker/id_test.py
+++ docker/id_test.py
@@ -4,53 +4,58 @@ import sys
 
 
 #### API adress
-api_address = '0.0.0.0'
+api_address = 'reverent_hodgkin'
 #### API port
 api_port = 8000
 
 #### Runtime vars
-username = sys.argv[1]
-password = sys.argv[2]
+username = ['alice', 'bob', 'clementine']
+password = ['wonderland', 'builder', 'mandarine']
 
 #### Request
-r = requests.get(
-    url='http://{address}:{port}/permissions'.format(address=api_address, port=api_port),
-    params= {
-        'username': username,
-        'password': password
-    }
-)
+for u in username:
+
+    index = username.index(u)
+    user = username[index]
+    pwd = password[index]
+
+    r = requests.get(
+        url='http://{address}:{port}/permissions'.format(address=api_address, port=api_port),
+        params= {
+            'username': user,
+            'password': pwd
+        }
+    )
 
 
-output = '''
-============================
-    Authentication Test
-============================
+    output = '''
+    ============================
+        Authentication Test
+    ============================
 
-request done at "/permissions"
-| username = {username}
-| password = {password}
+    request done at "/permissions"
+    | username = {user}
+    | password = {pwd}
 
-expected result = 200
-actual restult = {status_code}
+    expected result = 200
+    actual restult = {status_code}
 
-==>  {test_status}
+    ==>  {test_status}
 
-'''
+    '''
 
 
-# Request status
-status_code = r.status_code
+    #### Request status
+    status_code = r.status_code
 
-# Request result
-if status_code == 200:
-    test_status = 'SUCCESS'
-else:
-    test_status = 'FAILURE'
-print(output.format(username=sys.argv[1],password=sys.argv[2],status_code=status_code, test_status=test_status))
-
-# Write to log file
-if os.environ.get('LOG') == 1:
-    with open('api_test.log', 'a') as file:
-        file.write(output)
-
+    #### Request result
+    if status_code == 200:
+        test_status = 'SUCCESS'
+    else:
+        test_status = 'FAILURE'
+    print(output.format(user=user, pwd=pwd, status_code=status_code, test_status=test_status))
+    
+    #### Write to log file
+    if os.environ.get('LOG') == 1:
+        with open('api_test.log', 'a') as file:
+            file.write(output)
