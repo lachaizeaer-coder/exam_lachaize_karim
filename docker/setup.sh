f16abc0216138af86a9406f1717f2be80b1ce306
Ubuntu <ubuntu@ip-172-31-45-25.eu-west-1.compute.internal>
2026-07-23 08:20:40 +0000

    added docker setup // still not runned the script

--- docker/setup.sh
+++ docker/setup.sh
@@ -1,2 +1,13 @@
+# DOCKER SETUP
+docker volume create logs-volume
+
+# API
+docker image pull datascientest/fastapi:1.0.0
+docker container run -p 8000:8000 --name reverent_hodgkin --mount type=volume,src=logs-volume, dst=/home/logs datascientest/fastapi:1.0.0
+docker network connect api-tests reverent_hodgkin
+
+#
+
+# FIRST TEST
 docker image build . -f Dockerfile-id-test -t dockerfile-id-test:latest
 docker container run --mount type=volume, dst=/home/logs --network api-tests dockerfile-id-test:latest
