fef46d3625a70c0c4ee77ba0ec106675a1bcd56e
Ubuntu <ubuntu@ip-172-31-26-233.eu-west-1.compute.internal>
2026-09-05 09:40:46 +0000

    update variables

--- terraform/modules/ec2/variables.tf
+++ terraform/modules/ec2/variables.tf
@@ -1,10 +1,10 @@
 variable "instance_type" {
     description = "EC2 instance type"
-    type = string
-    default = "t3.micro"
+    type        = string
+    default     = "t3.micro"
 }
 
 variable "ami_id" {
     description = "AMI ID for the EC2 instance"
-    type = string
+    type        = string
 }
