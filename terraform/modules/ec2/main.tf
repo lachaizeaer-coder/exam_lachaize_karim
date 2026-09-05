ac22ebf704450847ec538bd1f41b9157487fcbac
Ubuntu <ubuntu@ip-172-31-26-233.eu-west-1.compute.internal>
2026-09-05 17:15:21 +0000

    add tag

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -2,4 +2,7 @@ resource "aws_instance" "ec2" {
   ami           = data.aws_ami.available_ami.id
   instance_type = var.instance_type
   availability_zone = data.aws_availability_zones.available.names[0]
+  tags = {
+    Name = "wordpress-ec2"
+  }
 }
