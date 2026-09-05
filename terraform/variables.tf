9ba12ac1796b78d5332adff22e56f5d11a7fde9a
Ubuntu <ubuntu@ip-172-31-26-233.eu-west-1.compute.internal>
2026-09-05 09:40:24 +0000

    update variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,20 +1,20 @@
 variable "aws_region" {
     description = "AWS region to deploy resources"
-    type = string
-    default = "eu-west-3"
+    type        = string
+    default     = "eu-west-3"
 }
 
 variable "aws_access_key" {
     description = "AWS access key"
-    type = string
-    default = ""
+    type        = string
+    default     = ""
     sensitive = true
 }
 
 variable "aws_secret_key" {
     description = "AWS secret key"
-    type = string
-    default = ""
-    sensitive = true
+    type        = string
+    default     = ""
+    sensitive   = true
 }
 
